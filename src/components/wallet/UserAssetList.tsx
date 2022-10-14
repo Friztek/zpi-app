2240c9a1082e9131b06c5b6ce8971516644c9fc5
Friztek <alfax166@gmail.com>
2022-10-14 15:06:08 +0200

    Stylize user asset component

--- src/components/wallet/UserAssetList.tsx
+++ src/components/wallet/UserAssetList.tsx
@@ -1,10 +1,30 @@
-import { createStyles, Text } from "@mantine/core";
+import {
+  ActionIcon,
+  Button,
+  createStyles,
+  Group,
+  Menu,
+  Stack,
+  Text,
+} from "@mantine/core";
+import {
+  IconArrowsLeftRight,
+  IconDotsVertical,
+  IconMessageCircle,
+  IconMinus,
+  IconPhoto,
+  IconPlus,
+  IconSearch,
+  IconSettings,
+  IconTrash,
+} from "@tabler/icons";
 
 const useStyles = createStyles((theme) => ({
   item: {
     ...theme.fn.focusStyles(),
     display: "flex",
     alignItems: "center",
+    justifyContent: "space-between",
     borderRadius: theme.radius.md,
     border: `1px solid ${
       theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[2]
@@ -20,16 +40,15 @@ const useStyles = createStyles((theme) => ({
   },
 
   symbol: {
-    fontSize: 30,
+    fontSize: 24,
     fontWeight: 700,
-    width: 60,
+    width: 70,
   },
 }));
 
 interface DndListProps {
   data: {
-    position: number;
-    mass: number;
+    category: string;
     symbol: string;
     name: string;
   }[];
@@ -40,17 +59,48 @@ export function UserAssetList({ data }: DndListProps) {
 
   const items = data.map((item) => (
     <div className={classes.item} key={item.name}>
-      <Text className={classes.symbol}>{item.symbol}</Text>
-      <div>
-        <Text>{item.name}</Text>
-        <Text color="dimmed" size="sm">
-          Position: {item.position} • Mass: {item.mass}
-        </Text>
+      <div style={{ display: "flex" }}>
+        <Text className={classes.symbol}>{item.symbol}</Text>
+        <div>
+          <Text>{item.name}</Text>
+          <Text color="dimmed" size="sm">
+            {item.category}
+          </Text>
+        </div>
       </div>
+      <Group>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            marginRight: 8,
+          }}
+        >
+          <Text size={24} weight={600}>
+            $3004
+          </Text>
+          <Text size={16} color="dimmed">
+            1000 zł
+          </Text>
+        </div>
+        <Menu shadow="md" width={120}>
+          <Menu.Target>
+            <ActionIcon size="sm">
+              <IconDotsVertical />
+            </ActionIcon>
+          </Menu.Target>
+
+          <Menu.Dropdown>
+            <Menu.Item icon={<IconPlus size={14} />}>Add</Menu.Item>
+            <Menu.Item icon={<IconMinus size={14} />}>
+              Subtract
+            </Menu.Item>
+            <Menu.Item icon={<IconTrash size={14} />}>Remove</Menu.Item>
+          </Menu.Dropdown>
+        </Menu>
+      </Group>
     </div>
   ));
 
-  return (
-    <div>{items}</div>
-  );
+  return <div>{items}</div>;
 }
--- src/pages/wallet.tsx
+++ src/pages/wallet.tsx
@@ -8,34 +8,19 @@ import { WalletValue } from "../components/wallet/WalletValue";
 
 const data = [
   {
-    position: 6,
-    mass: 12.011,
-    symbol: "C",
-    name: "Carbon",
+    category: "Currency",
+    symbol: "ZŁ",
+    name: "Zloty",
   },
   {
-    position: 7,
-    mass: 14.007,
-    symbol: "N",
-    name: "Nitrogen",
+    category: "Currency",
+    symbol: "EUR",
+    name: "Euro",
   },
   {
-    position: 39,
-    mass: 88.906,
-    symbol: "Y",
-    name: "Yttrium",
-  },
-  {
-    position: 56,
-    mass: 137.33,
-    symbol: "Ba",
-    name: "Barium",
-  },
-  {
-    position: 58,
-    mass: 140.12,
-    symbol: "Ce",
-    name: "Cerium",
+    category: "Currency",
+    symbol: "GBP",
+    name: "Great Britain Pound",
   },
 ];
 
