f4b30b701930a4a017b11aca414b3a0164b6a46a
Martyna Grzegorczyk <martyna.grzegorczyk@gmail.com>
2022-11-03 19:29:47 +0100

    fix: apexchart created

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Layout } from "../components/layout/Layout";
+import { Chart } from "../components/dashboard/Chart";
+
+const Dashboard = () => {
+  return (
+    <Layout>
+     <Chart />
+    </Layout>
+  );
+};
+
+export default Dashboard;
