7ca9e931f5b953efff2339d7e9205f77138556c0
Martyna Grzegorczyk <martyna.grzegorczyk@gmail.com>
2022-11-03 19:31:01 +0100

    fix: prevent dashboard from unauthorized

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Layout } from "../components/layout/Layout";
 import { Chart } from "../components/dashboard/Chart";
+import { withAuthenticationRequired } from "@auth0/auth0-react";
 
 const Dashboard = () => {
   return (
@@ -10,4 +11,7 @@ const Dashboard = () => {
   );
 };
 
-export default Dashboard;
+
+export default withAuthenticationRequired(Dashboard, {
+    onRedirecting: () => <p>Redirecting to the login page...</p>,
+  });
