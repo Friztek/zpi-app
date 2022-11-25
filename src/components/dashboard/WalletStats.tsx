2ff0459303537b31901d3b911ad59829af6f054f
Friztek <alfax166@gmail.com>
2022-11-25 14:02:21 +0100

    Fix user statistics not showing

--- src/components/dashboard/WalletStats.tsx
+++ src/components/dashboard/WalletStats.tsx
@@ -1,8 +1,11 @@
 import { createStyles, Progress, Box, Text, Group, Paper, SimpleGrid, Loader, ThemeIcon, Center } from "@mantine/core";
 import { IconArrowDownRight, IconArrowUpRight, IconDeviceAnalytics } from "@tabler/icons";
+import { format, lastDayOfMonth, sub } from "date-fns";
+import { isFinite } from "lodash";
 import { useQuery } from "react-query";
 import { useAPICommunication } from "../../contexts/APICommunicationContext";
-import { numberToMoneyString } from "../../utils/utils-format";
+import { lastDayOfPreviousMonth } from "../../utils/date-utils";
+import { dateToOffsetDate, numberToMoneyString } from "../../utils/utils-format";
 
 const useStyles = createStyles((theme) => ({
   progressLabel: {
@@ -38,26 +41,24 @@ interface WalletStatsProps {
 
 export const WalletStats = ({ userPreferenceCurrency }: WalletStatsProps) => {
   const { classes } = useStyles();
-
   const context = useAPICommunication();
 
   const walletTotalValueQuery = useQuery("walletTotalValue", async () => {
     return await context.walletApi.apiWalletTotalGet();
   });
 
-  const lastDayOfPrevMonth = () => {
-    var lastDayOfPrevMonth = new Date();
-    lastDayOfPrevMonth.setDate(1);
-    lastDayOfPrevMonth.setHours(-1);
-    return lastDayOfPrevMonth.toISOString().split("T", 1)[0];
-  };
-
   const walletLastMonthTotalValueQuery = useQuery("walletLastMonthTotalValue", async () => {
-    const data = await context.walletApi.apiWalletGet({ from: lastDayOfPrevMonth(), to: lastDayOfPrevMonth() });
-    return data[0].value;
+    const lastDayOfMonth = lastDayOfPreviousMonth();
+    const formattedLastDayOfMonth = format(lastDayOfMonth, "yyyy-MM-dd");
+    return await context.walletApi.apiWalletGet({ from: formattedLastDayOfMonth, to: formattedLastDayOfMonth });
   });
 
-  if (walletTotalValueQuery.data === undefined || walletLastMonthTotalValueQuery.data === undefined) {
+  if (
+    walletTotalValueQuery.isLoading ||
+    walletTotalValueQuery.data === undefined ||
+    walletLastMonthTotalValueQuery.isLoading ||
+    walletLastMonthTotalValueQuery.data === undefined
+  ) {
     return (
       <Paper withBorder style={{ height: 100 }} radius="md">
         <Center style={{ height: 100 }}>
@@ -72,27 +73,29 @@ export const WalletStats = ({ userPreferenceCurrency }: WalletStatsProps) => {
   const data = [
     {
       label: "Currency",
-      count: (Math.round(currencyTotalValue * 100) / 100),
+      count: Math.round(currencyTotalValue * 100) / 100,
       part: Math.round((currencyTotalValue / totalValue) * 10000) / 100,
       color: "#136a8a",
     },
     {
       label: "Crypto Currency",
-      count: (Math.round(cryptoTotalValue * 100) / 100),
+      count: Math.round(cryptoTotalValue * 100) / 100,
       part: Math.round((cryptoTotalValue / totalValue) * 10000) / 100,
       color: "#267871",
     },
     {
       label: "Metals",
-      count: (Math.round(metalTotalValue * 100) / 100),
+      count: Math.round(metalTotalValue * 100) / 100,
       part: Math.round((metalTotalValue / totalValue) * 10000) / 100,
       color: "#00bf8f",
     },
   ];
 
-  const diff =
-    Math.round(((totalValue - walletLastMonthTotalValueQuery.data) / walletLastMonthTotalValueQuery.data) * 10000) /
-    100;
+  const lastMonthWalletData = walletLastMonthTotalValueQuery.data[0];
+
+  const diff = lastMonthWalletData
+    ? Math.round(((totalValue - lastMonthWalletData.value) / lastMonthWalletData.value) * 10000) / 100
+    : null;
 
   const segments = data.map((segment) => ({
     value: segment.part,
@@ -109,9 +112,11 @@ export const WalletStats = ({ userPreferenceCurrency }: WalletStatsProps) => {
         <Text weight={700}>
           {numberToMoneyString(stat.count)} {userPreferenceCurrency}
         </Text>
-        <Text color={stat.color} weight={700} size="sm" className={classes.statCount}>
-          {stat.part}%
-        </Text>
+        {isFinite(stat.part) && (
+          <Text color={stat.color} weight={700} size="sm" className={classes.statCount}>
+            {stat.part}%
+          </Text>
+        )}
       </Group>
     </Box>
   ));
@@ -119,38 +124,46 @@ export const WalletStats = ({ userPreferenceCurrency }: WalletStatsProps) => {
   const roundedTotalValue = Math.round(totalValue * 100) / 100;
   return (
     <Paper withBorder p="lg" radius="md">
-      <Group position="apart">
-        <Group align="flex-end" spacing="xs">
-          <Text size="xl" weight={700}>
-            { numberToMoneyString(roundedTotalValue) } {userPreferenceCurrency}
-          </Text>
-          <Text color="teal" className={classes.diff} size="sm" weight={700}>
-            <span>{ numberToMoneyString(diff) }%</span>
-            <ThemeIcon
-              color="gray"
-              variant="light"
-              sx={(theme) => ({
-                color: diff! > 0 ? theme.colors.teal[6] : theme.colors.red[6],
-              })}
-              size={38}
-              radius="md"
-            >
-              {diff > 0 ? (
-                <IconArrowUpRight size={25} stroke={1.5}></IconArrowUpRight>
-              ) : (
-                <IconArrowDownRight size={25} stroke={1.5}></IconArrowDownRight>
-              )}
-            </ThemeIcon>
-          </Text>
-        </Group>
-        <IconDeviceAnalytics size={20} className={classes.icon} stroke={1.5} />
-      </Group>
+      {diff ? (
+        <>
+          <Group position="apart">
+            <Group align="flex-end" spacing="xs">
+              <Text size="xl" weight={700}>
+                {numberToMoneyString(roundedTotalValue)} {userPreferenceCurrency}
+              </Text>
+              <Text color="teal" className={classes.diff} size="sm" weight={700}>
+                <span>{numberToMoneyString(diff)}%</span>
+                <ThemeIcon
+                  color="gray"
+                  variant="light"
+                  sx={(theme) => ({
+                    color: diff! > 0 ? theme.colors.teal[6] : theme.colors.red[6],
+                  })}
+                  size={38}
+                  radius="md"
+                >
+                  {diff > 0 ? (
+                    <IconArrowUpRight size={25} stroke={1.5}></IconArrowUpRight>
+                  ) : (
+                    <IconArrowDownRight size={25} stroke={1.5}></IconArrowDownRight>
+                  )}
+                </ThemeIcon>
+              </Text>
+            </Group>
+            <IconDeviceAnalytics size={20} className={classes.icon} stroke={1.5} />
+          </Group>
 
-      <Text color="dimmed" size="sm">
-        Total wallet value compared to previous month
-      </Text>
-      
-      <Progress sections={segments} size={34} classNames={{ label: classes.progressLabel }} mt={40} />
+          <Text color="dimmed" size="sm">
+            Total wallet value compared to previous month
+          </Text>
+          <Progress sections={segments} size={34} classNames={{ label: classes.progressLabel }} mt={40} />
+        </>
+      ) : (
+        <Text size="xl" weight={400}>
+          Your wallet is empty, add some assets!
+        </Text>
+      )}
+
       <SimpleGrid cols={1} mt="xl" mb="md">
         {descriptions}
       </SimpleGrid>
