dd5a40fa6d7719acc557d21049e8cba17a261a92
Friztek <alfax166@gmail.com>
2022-12-06 10:13:37 +0100

    Fix user preference not loading

--- src/components/profile/UserPreferences.tsx
+++ src/components/profile/UserPreferences.tsx
@@ -1,34 +1,33 @@
-import { Button, Group, Paper, Text, ActionIcon, createStyles, Switch, Loader, Center } from "@mantine/core";
-import { IconEdit, IconEditOff, IconTools } from "@tabler/icons";
-import { useState } from "react";
-import { CurrencySwitch } from "./CurrencySwitch";
-import { useAPICommunication } from "../../contexts/APICommunicationContext";
-import { useMutation, useQuery, useQueryClient } from "react-query";
-import { UpdateUserPreferencesRequest, UserPreferencesDto } from "../../client-typescript";
+import { Button, Group, Paper, Text, ActionIcon, createStyles, Switch, Loader, Center } from '@mantine/core';
+import { IconEdit, IconEditOff, IconTools } from '@tabler/icons';
+import { useState } from 'react';
+import { CurrencySwitch } from './CurrencySwitch';
+import { useAPICommunication } from '../../contexts/APICommunicationContext';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { UpdateUserPreferencesRequest, UserPreferencesDto } from '../../client-typescript';
 
 const useStyles = createStyles((theme) => ({
   switch: {
-    marginTop: 10,
+    marginTop: 10
   },
   label: {
-    color: `${theme.colorScheme === "dark" ? theme.white : theme.colors.dark[6]} !important`,
-  },
+    color: `${theme.colorScheme === 'dark' ? theme.white : theme.colors.dark[6]} !important`
+  }
 }));
 
 export function UserPreferences() {
   const { classes } = useStyles();
+  const [userPreferences, setUserPreferences] = useState<UserPreferencesDto>();
 
   const context = useAPICommunication();
 
-  const [userPreferences, setUserPreferences] = useState<UserPreferencesDto>();
+  const queryClient = useQueryClient();
 
-  const userPreferencesQuery = useQuery("userPreferences", async () => {
+  const userPreferencesQuery = useQuery('userPreference', async () => {
     const data = await context.userPreferenceAPI.getUserPreferences();
     setUserPreferences(data);
     return data;
   });
-
-  const queryClient = useQueryClient();
 
   const mutation = useMutation(
     (patchUserPreferencesDto: UpdateUserPreferencesRequest) => {
@@ -36,34 +35,33 @@ export function UserPreferences() {
     },
     {
       onSuccess: () => {
-        queryClient.invalidateQueries("userPreferences");
-      },
+        queryClient.invalidateQueries('userPreference');
+      }
     }
   );
 
   const [isDisabled, setIsDisabled] = useState(true);
 
   const changeIsDisabled = () => {
-    if(isDisabled === false) {
+    if (isDisabled === false) {
       setUserPreferences(userPreferencesQuery.data);
     }
     setIsDisabled(!isDisabled);
   };
 
   const saveForm = () => {
-    mutation.mutate({updateUserPreferencesDto: userPreferences});
+    mutation.mutate({ updateUserPreferencesDto: userPreferences });
     setIsDisabled(!isDisabled);
   };
 
   return (
-    <Paper withBorder p="md" shadow="md" radius="md" style={{minHeight:200}}>
+    <Paper withBorder p="md" shadow="md" radius="md" style={{ minHeight: 200 }}>
       <Group
         style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-        }}
-      >
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between'
+        }}>
         <Group>
           <IconTools stroke={1.5} size={20} />
           <Text size="xl">Preferences</Text>
@@ -73,13 +71,12 @@ export function UserPreferences() {
           onClick={() => {
             changeIsDisabled();
           }}
-          size="sm"
-        >
+          size="sm">
           {isDisabled ? <IconEdit /> : <IconEditOff />}
         </ActionIcon>
       </Group>
       {userPreferences === undefined ? (
-        <Center h={120}> 
+        <Center h={120}>
           <Loader size="xl" variant="dots" />
         </Center>
       ) : (
@@ -95,7 +92,7 @@ export function UserPreferences() {
             label="Do you want to recieve alerts on email?"
             checked={userPreferences.alertsOnEmail}
             onChange={(event) => setUserPreferences((prev) => ({ ...prev!, alertsOnEmail: event.target.checked }))}
-            styles={{ label: { color: " !important" } }}
+            styles={{ label: { color: ' !important' } }}
           />
           <Switch
             disabled={isDisabled}
@@ -103,10 +100,10 @@ export function UserPreferences() {
             label="Do you want to recieve weekly reports on email?"
             checked={userPreferences.weeklyReports}
             onChange={(event) => {
-              console.log("event", event);
-              setUserPreferences((prev) => ({ ...prev!, weeklyReports: event.target.checked }))}
-            }
-              />
+              console.log('event', event);
+              setUserPreferences((prev) => ({ ...prev!, weeklyReports: event.target.checked }));
+            }}
+          />
 
           {!isDisabled && (
             <Group position="center" mt="xl">
